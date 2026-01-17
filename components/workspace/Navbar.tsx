fb92fe7b122f7da16e0ce6c71860ec09e4130392
vinngo <ngovincent9593@gmail.com>
2026-01-17 15:08:13 -0800

    feat(ui): add Navbar for visual organization

---
+++ components/workspace/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className="flex border-b">
+      <h1 className="text-4xl p-3 font-semibold text-gray-900 tracking-tight">
+        Socratical
+      </h1>
+    </div>
+  );
+}
