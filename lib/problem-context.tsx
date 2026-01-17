838f5077d28f5a4ae2a7d5afa9ebe70a09d33037
vinngo <ngovincent9593@gmail.com>
2026-01-16 23:11:58 -0800

    feat(02-01): add problem state management using context

---
+++ lib/problem-context.tsx
@@ -0,0 +1,62 @@
+import { useContext, createContext, useState } from "react";
+
+export type ProblemContextType = {
+  problemText: string | null;
+  problemImage: { url: string; file: File };
+  setProblem(text: string): void;
+  setProblemImage(file: File): void;
+  clearProblem(): void;
+};
+
+export const ProblemContext = createContext<ProblemContextType | null>(null);
+
+export const ProblemProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [problemText, setProblemText] = useState<string | null>(null);
+  const [problemImage, setProblemImageState] = useState<{
+    url: string;
+    file: File;
+  } | null>(null);
+
+  const setProblem = (text: string) => {
+    setProblemText(text);
+  };
+
+  const setProblemImage = (file: File) => {
+    const url = URL.createObjectURL(file);
+    setProblemImageState({ url, file });
+  };
+
+  const clearProblem = () => {
+    setProblemText(null);
+    if (problemImage) {
+      URL.revokeObjectURL(problemImage.url);
+    }
+    setProblemImageState(null);
+  };
+
+  return (
+    <ProblemContext.Provider
+      value={{
+        problemText,
+        problemImage: problemImage || { url: "", file: new File([], "") },
+        setProblem,
+        setProblemImage,
+        clearProblem,
+      }}
+    >
+      {children}
+    </ProblemContext.Provider>
+  );
+};
+
+export const useProblem = () => {
+  const context = useContext(ProblemContext);
+  if (!context) {
+    throw new Error("useProblemContext must be used within a ProblemProvider");
+  }
+  return context;
+};
